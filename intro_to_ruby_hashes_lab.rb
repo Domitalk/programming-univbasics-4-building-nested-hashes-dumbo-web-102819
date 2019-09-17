6341684b40edb76925c254fee5f5af3aca6dff33
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-17 02:32:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,10 +17,10 @@ def monopoly_with_third_tier
 		:railroads => {
 			:pieces => 4,
 			:rent_in_dollars => {
-				:own1 => 25,
-				:own2 => 50,
-				:own3 => 100,
-				:own4 => 200
+				:one_piece_owned => 25,
+				:two_pieces_owned => 50,
+				:three_pieces_owned => 100,
+				:four_pieces_owned => 200
 			},
 			:names => {
 				:rr1 => "Reading Railroad",
